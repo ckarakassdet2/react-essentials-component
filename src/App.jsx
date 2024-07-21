780068be20af30a99b2dba96f918ad447be13be4
Jack Cahit Karakas <ckarakassdet@gmail.com>
2024-07-21 18:53:27 -0500

    re-use components with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 
+import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max){
@@ -9,7 +12,7 @@ function Header(){
   const description = reactDescriptions[genRandomInt(2)]; 
 
   return (<header>
-    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <img src={reactImg} alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
       {description} React concepts you will need for almost any app you are
@@ -18,14 +21,47 @@ function Header(){
   </header>); 
 }
 
+function CoreConcept({image, title, description}){
+  return(
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+
+    </li>
+  );
+}
+
+export function CourseGoal(props) {
+  return (
+    <li>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header></Header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}></CoreConcept>
+          <CoreConcept {...CORE_CONCEPTS[1]} ></CoreConcept>
+          <CoreConcept {...CORE_CONCEPTS[2]}></CoreConcept>
+          <CoreConcept {...CORE_CONCEPTS[3]}></CoreConcept>
+        </ul>
+        </section>
+        <div id='core-concepts'>
+        <CourseGoal title="Learn React" description="In-depth." ></CourseGoal>
+        <CourseGoal title="Practice" description="Practice working with React components." ></CourseGoal>
+        </div>
       </main>
     </div>
+    
   );
 }
 
