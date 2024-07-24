cfebaac0abfbd01303fbfbebe28f7829b0b72a97
Jack Cahit Karakas <ckarakassdet@gmail.com>
2024-07-24 17:02:51 -0500

    updated coreconcept dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -49,10 +49,7 @@ function App() {
         <section id='core-concepts'>
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}></CoreConcept>
-            <CoreConcept {...CORE_CONCEPTS[1]} ></CoreConcept>
-            <CoreConcept {...CORE_CONCEPTS[2]}></CoreConcept>
-            <CoreConcept {...CORE_CONCEPTS[3]}></CoreConcept>
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} ></CoreConcept> )} 
           </ul>
         </section>
         <section id='examples'>
