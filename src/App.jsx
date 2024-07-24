3d857ef095b7c4beec9933338845ce6d96aac412
Jack Cahit Karakas <ckarakassdet@gmail.com>
2024-07-24 01:34:20 -0500

    added rendering content conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
-
+import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data-with-examples.js';
 
 
 export function CourseGoal(props) {
@@ -14,26 +16,58 @@ export function CourseGoal(props) {
 }
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+    // console.log(selectedTopic); 
+  }
+
+  console.log("APP COMPONENT EXECUTED.");
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>
+    )
+  }
+
   return (
     <div>
       <Header></Header>
       <main>
         <section id='core-concepts'>
-        <h2>Core Concepts</h2>
-        <ul>
-          <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}></CoreConcept>
-          <CoreConcept {...CORE_CONCEPTS[1]} ></CoreConcept>
-          <CoreConcept {...CORE_CONCEPTS[2]}></CoreConcept>
-          <CoreConcept {...CORE_CONCEPTS[3]}></CoreConcept>
-        </ul>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}></CoreConcept>
+            <CoreConcept {...CORE_CONCEPTS[1]} ></CoreConcept>
+            <CoreConcept {...CORE_CONCEPTS[2]}></CoreConcept>
+            <CoreConcept {...CORE_CONCEPTS[3]}></CoreConcept>
+          </ul>
         </section>
-        <div id='core-concepts'>
-        <CourseGoal title="Learn React" description="In-depth." ></CourseGoal>
-        <CourseGoal title="Practice" description="Practice working with React components." ></CourseGoal>
-        </div>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          { tabContent }
+        </section>
       </main>
     </div>
-    
+
   );
 }
 
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,7 @@
+
+export default function TabButton({children, onSelect}){
+   console.log("TABBUTTON COMPONENT EXECUTED."); 
+    return (
+        <li><button onClick={onSelect}>{children}</button></li>
+    );
+}
